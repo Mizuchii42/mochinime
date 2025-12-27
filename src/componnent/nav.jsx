d3cd7b2a2dc162e8e5a446b378373086007a4f46
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-27 18:35:35 +0700

    add version

--- src/componnent/nav.jsx
+++ src/componnent/nav.jsx
@@ -5,7 +5,10 @@ const Navbar = () => {
   return (
     <>
       <div className="w-full h-16 flex justify-items-center backdrop-blur-sm items-center fixed mt-0 mb-0 justify-between">
-        <h1 className="font-extrabold text-sky-500 ml-3">mochinime</h1>
+        <div>
+          <h1 className="font-extrabold text-sky-500 ml-3">mochinime</h1>
+          <small className="ml-3 text-gray-500">V.1.0.2</small>
+        </div>
         <div className="mr-5 flex gap-3">
           <Link to="/"><Home color="oklch(68.5% 0.169 237.323)" size="25px" /></Link>
           <Link to="/search"><Search color="oklch(68.5% 0.169 237.323)" size="25px" /></Link>
